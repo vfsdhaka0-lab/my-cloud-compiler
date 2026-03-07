e89dcfb4fd66b9ed437eae8d2667896041a338b1
vfsdhaka0-lab <vfsdhaka0@gmail.com>
2026-03-08 05:56:55 +0600

    Cloud Sync

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text('Róyal Cloud IDE')),
+      appBar: AppBar(title: Text('Róyal Event')),
       body: Center(child: Text('Build Successful!')), 
     ),
   ));
