722fa144ea11facab514b762a2721dc9aff3fd4a
vfsdhaka0-lab <vfsdhaka0@gmail.com>
2026-03-09 03:00:24 +0600

    Sync from Róyal IDE

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,2 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text('Róyal Event'),
-      ),
-    ),
-  ));
-}
+void main() => runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('Róyal €vent')))));
